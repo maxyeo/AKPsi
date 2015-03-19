59569540b9ed287c262351003ddeb59b580f7162
Max Yeo <mzwyeo@gmail.com>
2015-03-19 11:32:40 -0400

    bio overlay

--- js/style.js
+++ js/style.js
@@ -4,9 +4,44 @@ $(document).ready(function() {
 
     //nav bar scoll action
     $('#logo, #ship').click(function() {
-        $("html, body").animate({ scrollTop: $('#home').offset().top}, 500);
+        $("html, body").animate({ scrollTop: $('.top').offset().top}, 500);
     });//clicked on top
-    
+
+    $('.bio').click(function() {
+      $('#bios').addClass('active');
+      var image = $(this).attr("data-image");
+      var pledge_class = $(this).attr("data-class");
+      var pledge_class_greek = $(this).attr("data-class-greek");
+      var name = $(this).attr("data-name");
+      var position = $(this).attr("data-position");
+      var major = $(this).attr("data-major");
+      var bio = $(this).attr("data-bio");
+      $('#bios-bro-pic').css('background-image', 'url(' + image + ')');
+      $("#bios h4").html(name);
+      $("#bios h5").html(position);
+      $("#bios h6").html(major);
+      $("#bios-bio").html(bio);
+      $("#greek span").html(pledge_class_greek);
+      $("#greek h6").html(pledge_class);
+    });
+
+    $('#bios-wrapper').click(function(e){
+      e.stopPropagation();
+    });
+
+    $('#bios, #bios-out').click(function(){
+      $('#bios').removeClass('active');
+      setTimeout(function () {
+        $('#bios-bro-pic').css('background-image', 'url()');
+        $("#bios h4").html("");
+        $("#bios h5").html("");
+        $("#bios h6").html("");
+        $("#bios-bio").html("");
+        $("#greek span").html("");
+        $("#greek h6").html("");
+      }, 300);
+    });
+
 });
 
 $(window).resize(function() {
@@ -48,15 +83,4 @@ $(function() {
 $(".navicon-button").click(function(){
   $("nav").toggleClass("open");
   $(this).toggleClass("open");
-});
-
-$(".member .member-pic").click(function(e){
-  e.stopPropagation();
-  var member = $(this).parent(".member");
-  member.children(".member-bio").addClass("open");
-});
-
-$(".member-out").click(function(e){
-  e.stopPropagation();
-  $(".member-bio").removeClass("open");
 });
